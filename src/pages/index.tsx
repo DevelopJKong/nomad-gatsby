9528b65e4c4d418952341b0e29f4134703952078
cafe small house <ybaram40@gmail.com>
2023-01-04 07:49:23 +0900

    #10.10 Page Query 완료

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
 import * as React from 'react';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
-
-const IndexPage = () => {
+import { PageProps, graphql } from 'gatsby';
+const IndexPage = ({ data }: PageProps<Queries.BlogTitlesQuery>) => {
+   console.log(data);
    return (
       <Layout title='Welcome to DevSticker'>
-         <div></div>
+         <ul>
+            {data.allFile.nodes.map((file, index) => (
+               <li key={index}>{file.name}</li>
+            ))}
+         </ul>
       </Layout>
    );
 };
+
+export const query = graphql`
+   query BlogTitles {
+      allFile {
+         nodes {
+            name
+         }
+      }
+   }
+`;
 
 export const Head = () => <Seo title='Home' />;
 
